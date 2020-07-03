d745df920d3377c18e189c892ed0a3340698cf42
hareeshcs33 <hareeshcs33@gmail.com>
2020-07-03 09:54:31 +0530

    project initial commit

---
+++ src/components/quote-layout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>Quote layout content goes here!!!</h1>
+        <hr />
+        
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+        }
+    },
+    methods: {
+    },
+    components: {
+    }
+}
+</script>
+<style>
+
+</style>
